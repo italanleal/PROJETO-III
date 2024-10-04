9e0bce91995234effb82888ab410b8e61f0f6059
italanleal <italanleal@gmail.com>
2024-10-04 09:36:05 -0300

    aCommit

--- ui/src/main/java/br/upe/UserInterface/App.java
+++ ui/src/main/java/br/upe/UserInterface/App.java
@@ -2,7 +2,7 @@ package br.upe.UserInterface;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World, 2");
         MainMenu mainMenu = new MainMenu();
         mainMenu.displayStartMenu();
     }
