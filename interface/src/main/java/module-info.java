eea494347caab7c133087b70e730971867fbd20d
italanleal <italanleal@gmail.com>
2024-10-21 13:20:03 -0300

    aCommit

--- interface/src/main/java/module-info.java
+++ interface/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module br.upe.UserInterface {
     requires javafx.controls;
     requires javafx.fxml;
     requires br.upe.controllers;
+    requires persistencia;
 
     exports br.upe.UserInterface;
     opens br.upe.UserInterface to javafx.fxml;
