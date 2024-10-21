6544526a1cc3c7e0d9085a066424ee4b1fd1fef1
italanleal <italanleal@gmail.com>
2024-10-20 21:43:04 -0300

    aCommit

--- interface/src/main/java/br/upe/UserInterface/RegisterController.java
+++ interface/src/main/java/br/upe/UserInterface/RegisterController.java
@@ -2,8 +2,33 @@ package br.upe.UserInterface;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
 public class RegisterController {
+    @FXML
+    TextField emailField;
+    @FXML
+    TextField passwordField;
+    @FXML
+    CheckBox adminStatus;
+
+    @FXML
+    private void register() throws IOException {
+        String email = emailField.getText();
+        String password = passwordField.getText();
+
+        boolean isAdmin = adminStatus.isSelected();
+        boolean isCreated = false;
+
+        if(isAdmin) {
+            isCreated = AppStateController.authController.createNewAdmin(email, password);
+        } else {
+            isCreated = AppStateController.authController.createNewUser(email, password);
+        }
+
+        if(isCreated) switchToLogin();
+    }
 
     @FXML
     private void switchToLogin() throws IOException {
