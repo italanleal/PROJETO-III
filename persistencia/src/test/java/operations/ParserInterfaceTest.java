0a3a5e7dd5390af9ee8b40ca9aeab0f567ad46a9
Jucesa <jc16032005@gmail.com>
2024-10-22 12:06:10 -0300

    testes completos ParserInterfaceTest

--- persistencia/src/test/java/operations/ParserInterfaceTest.java
+++ persistencia/src/test/java/operations/ParserInterfaceTest.java
@@ -1,11 +1,7 @@
 package operations;
 
-import br.upe.operations.CRUDInterface;
-import br.upe.operations.ParserInterface;
-import br.upe.operations.UserCRUD;
-import br.upe.pojos.KeeperInterface;
-import br.upe.pojos.Submission;
-import br.upe.pojos.User;
+import br.upe.operations.*;
+import br.upe.pojos.*;
 import org.junit.jupiter.api.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,7 +15,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class ParserInterfaceTest {
     private static final String STATE_PATH = ".\\state";
     private static final String USERS_PATH = STATE_PATH+"\\users.csv";
+    private static final String EVENTS_PATH = STATE_PATH+"\\events.csv";
+    private static final String SUBSCRIPTIONS_PATH = STATE_PATH+"\\subscriptions.csv";
+    private static final String SESSIONS_PATH = STATE_PATH+"\\sessions.csv";
     private static final String SUBMISSIONS_PATH = STATE_PATH+"\\submissions.csv";
+
     Logger logger = Logger.getLogger(ParserInterface.class.getName());
 
     @AfterEach
@@ -27,6 +27,9 @@ class ParserInterfaceTest {
         try {
             Files.deleteIfExists(Paths.get(SUBMISSIONS_PATH));
             Files.deleteIfExists(Paths.get(USERS_PATH));
+            Files.deleteIfExists(Paths.get(EVENTS_PATH));
+            Files.deleteIfExists(Paths.get(SESSIONS_PATH));
+            Files.deleteIfExists(Paths.get(SUBSCRIPTIONS_PATH));
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Error when trying to delete files", e);
         }
@@ -106,4 +109,84 @@ class ParserInterfaceTest {
         }
     }
 
+    @Test
+    void testParseSubscription_ValidInput() {
+        UUID subscriptionId = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+        UUID sessionId = UUID.randomUUID();
+        Subscription subscription = KeeperInterface.createSubscription();
+        subscription.setUuid(subscriptionId);
+        subscription.setUserUuid(userId);
+        subscription.setSessionUuid(sessionId);
+        subscription.setDate(new Date());
+        SubscriptionCRUD subscriptionCRUD = CRUDInterface.newSubscriptionCRUD();
+        subscriptionCRUD.createSubscription(subscription);
+
+        try(BufferedReader br = new BufferedReader(new FileReader(SUBSCRIPTIONS_PATH))){
+            while(br.ready()){
+                String line = br.readLine();
+                Subscription retrievedSubscription = ParserInterface.parseSubscription(line);
+                assertNotNull(retrievedSubscription, "The parsed user was null");
+                assertEquals(subscriptionId, retrievedSubscription.getUuid());
+                assertEquals(userId, retrievedSubscription.getUserUuid());
+                assertEquals(sessionId, retrievedSubscription.getSessionUuid());
+                assertEquals(subscription.getDate().toInstant(), retrievedSubscription.getDate().toInstant());
+            }
+        } catch (IOException e){
+            logger.log(Level.SEVERE, "Error when trying to read users.csv file", e);
+        }
+    }
+
+    @Test
+    void testParseSession_ValidInput() {
+        UUID sessionId = UUID.randomUUID();
+        UUID eventId = UUID.randomUUID();
+        Session session = KeeperInterface.createSession();
+        session.setSubscriptions(new ArrayList<>());
+        session.setUuid(sessionId);
+        session.setEventUuid(eventId);
+        session.setDescritor("descritor");
+        session.setStartDate(new Date());
+        session.setEndDate(new Date());
+        SessionCRUD sessionCRUD = CRUDInterface.newSessionCRUD();
+        sessionCRUD.createSession(session);
+
+        try(BufferedReader br = new BufferedReader(new FileReader(SESSIONS_PATH))){
+            while(br.ready()){
+                String line = br.readLine();
+                Session retrievedSession = ParserInterface.parseSession(ParserInterface.validadeString(line));
+                assertNotNull(retrievedSession, "The parsed session was null");
+                assertEquals(sessionId, retrievedSession.getUuid());
+                assertEquals(eventId, retrievedSession.getEventUuid());
+            }
+        } catch (IOException e){
+            logger.log(Level.SEVERE, "Error when trying to read users.csv file", e);
+        }
+    }
+
+    @Test
+    void testParseEvent_ValidInput() {
+        UUID eventId = UUID.randomUUID();
+        GreatEvent event = KeeperInterface.createGreatEvent();
+        event.setUuid(eventId);
+        event.setDescritor("descritor");
+        event.setStartDate(new Date());
+        event.setEndDate(new Date());
+        event.setSubmissions(new ArrayList<>());
+        event.setSessions(new ArrayList<>());
+        EventCRUD eventCRUD = CRUDInterface.newEventCRUD();
+        eventCRUD.createEvent(event);
+
+        try(BufferedReader br = new BufferedReader(new FileReader(EVENTS_PATH))){
+            while(br.ready()){
+                String line = br.readLine();
+                GreatEvent retrievedEvent = ParserInterface.parseEvent(ParserInterface.validadeString(line));
+                assertNotNull(retrievedEvent, "The parsed event was null");
+                assertEquals(eventId, retrievedEvent.getUuid());
+            }
+        } catch (IOException e){
+            logger.log(Level.SEVERE, "Error when trying to read users.csv file", e);
+        }
+
+    }
 }
