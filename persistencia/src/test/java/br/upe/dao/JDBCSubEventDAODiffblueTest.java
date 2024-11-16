2c5074e2615c470365aa32d6642ed856fb65bb4f
italanleal <italanleal@gmail.com>
2024-11-16 12:34:42 -0300

    aCommit

--- persistencia/src/test/java/br/upe/dao/JDBCSubEventDAODiffblueTest.java
+++ persistencia/src/test/java/br/upe/dao/JDBCSubEventDAODiffblueTest.java
@@ -1,5 +1,6 @@
 package br.upe.dao;
 
+import br.upe.entities.Session;
 import br.upe.entities.SubEvent;
 import br.upe.util.persistencia.LambdaEntityManagerFactory;
 import br.upe.util.persistencia.PersistenciaInterface;
@@ -24,13 +25,102 @@ class JDBCSubEventDAODiffblueTest {
     public void saveEventTest() {
         SubEvent subEvent = PersistenciaInterface.createSubEvent();
 
-        subEvent.setTitle("SUPER");
-        subEvent.setDescription("...");
+        subEvent.setTitle("TEST1");
+        subEvent.setDescription("TEST1");
         subEvent.setStartDate(LocalDate.now());
+        subEvent.setEndDate(LocalDate.now().plusDays(1));
 
         subEventDAO.save(subEvent);
 
         Assertions.assertNotEquals(subEventDAO.findById(subEvent.getId()), Optional.empty());
+    }
 
+    @Test
+    @DisplayName("Test update method for subEventDAO")
+    public void updateSubEventTest() {
+        SubEvent subEvent = PersistenciaInterface.createSubEvent();
+
+        subEvent.setTitle("TEST1");
+        subEvent.setDescription("TEST1");
+        subEvent.setStartDate(LocalDate.now());
+        subEvent.setEndDate(LocalDate.now().plusDays(1));
+
+        subEventDAO.save(subEvent);
+
+        subEvent.setTitle("TEST2");
+        subEvent.setDescription("TEST2");
+        subEvent.setStartDate(LocalDate.now().plusDays(1));
+        subEvent.setEndDate(LocalDate.now());
+
+        subEventDAO.update(subEvent);
+
+        Assertions.assertEquals(subEvent.getTitle(), "TEST2");
+    }
+
+    @Test
+    @DisplayName("Test delete method for subEventDAO")
+    public void deleteSubEventTest() {
+        SubEvent subEvent = PersistenciaInterface.createSubEvent();
+
+        subEvent.setTitle("TEST1");
+        subEvent.setDescription("TEST1");
+        subEvent.setStartDate(LocalDate.now());
+        subEvent.setEndDate(LocalDate.now().plusDays(1));
+
+        subEventDAO.save(subEvent);
+
+        Long id = subEvent.getId();
+        subEventDAO.delete(subEvent);
+        Assertions.assertEquals(subEventDAO.findById(id), Optional.empty());
+    }
+
+    @Test
+    @DisplayName("Test deleteById method for subEventDAO")
+    public void deleteByIdSubEventTest() {
+        SubEvent subEvent = PersistenciaInterface.createSubEvent();
+
+        subEvent.setTitle("TEST1");
+        subEvent.setDescription("TEST1");
+        subEvent.setStartDate(LocalDate.now());
+        subEvent.setEndDate(LocalDate.now().plusDays(1));
+
+        subEventDAO.save(subEvent);
+
+        Long id = subEvent.getId();
+        subEventDAO.deleteById(subEvent.getId());
+        Assertions.assertEquals(subEventDAO.findById(id), Optional.empty());
+    }
+
+    @Test
+    @DisplayName("Test findById method for subEventDAO")
+    public void findByIdSubEventTest() {
+        SubEvent subEvent = PersistenciaInterface.createSubEvent();
+
+        subEvent.setTitle("TEST1");
+        subEvent.setDescription("TEST1");
+        subEvent.setStartDate(LocalDate.now());
+        subEvent.setEndDate(LocalDate.now().plusDays(1));
+
+        subEventDAO.save(subEvent);
+
+        Long id = subEvent.getId();
+        Assertions.assertEquals(subEventDAO.findById(id).get(), subEvent);
+    }
+
+    @Test
+    @DisplayName("Test findAll method for subEventDAO")
+    public void findAllSubEventTest() {
+        SubEvent subEvent = PersistenciaInterface.createSubEvent();
+
+        subEvent.setTitle("TEST1");
+        subEvent.setDescription("TEST1");
+        subEvent.setStartDate(LocalDate.now());
+        subEvent.setEndDate(LocalDate.now().plusDays(1));
+
+        subEventDAO.save(subEvent);
+
+
+        List<SubEvent> subEvents = subEventDAO.findAll();
+        Assertions.assertFalse(subEvents.isEmpty());
     }
 }
--- persistencia/src/test/java/br/upe/dao/JDBCSubmissionDAODiffblueTest.java
+++ persistencia/src/test/java/br/upe/dao/JDBCSubmissionDAODiffblueTest.java
@@ -2,12 +2,14 @@ package br.upe.dao;
 
 import static org.mockito.Mockito.mock;
 
+import br.upe.entities.Certification;
 import br.upe.entities.Submission;
 import br.upe.util.persistencia.LambdaEntityManagerFactory;
 import br.upe.util.persistencia.PersistenciaInterface;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.*;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
@@ -27,8 +29,85 @@ class JDBCSubmissionDAODiffblueTest {
         Submission submission = PersistenciaInterface.createSubmission();
         submission.setFilename("article.txt");
         submission.setContent((new String("lots of data")).getBytes(StandardCharsets.UTF_8));
+        submission.setDate(LocalDate.now());
         submissionDAO.save(submission);
 
         Assertions.assertNotEquals(submissionDAO.findById(submission.getId()), Optional.empty());
+    }
+
+    @Test
+    @DisplayName("Test update method for submissionDAO")
+    public void updateSubmissionTest() {
+        Submission submission = PersistenciaInterface.createSubmission();
+        submission.setFilename("article.txt");
+        submission.setContent((new String("lots of data")).getBytes(StandardCharsets.UTF_8));
+        submission.setDate(LocalDate.now());
+        submissionDAO.save(submission);
+
+        submission.setFilename("article2.txt");
+        submission.setContent((new String("lots of data2")).getBytes(StandardCharsets.UTF_8));
+        submission.setDate(LocalDate.now());
+        submissionDAO.update(submission);
+
+        Assertions.assertEquals(submission.getFilename(), "article2.txt");
+    }
+
+    @Test
+    @DisplayName("Test delete method for submissionDAO")
+    public void deleteSubmissionTest() {
+        Submission submission = PersistenciaInterface.createSubmission();
+        submission.setFilename("article.txt");
+        submission.setContent((new String("lots of data")).getBytes(StandardCharsets.UTF_8));
+        submission.setDate(LocalDate.now());
+
+        submissionDAO.save(submission);
+
+        Long id = submission.getId();
+        submissionDAO.delete(submission);
+        Assertions.assertEquals(submissionDAO.findById(id), Optional.empty());
+    }
+
+    @Test
+    @DisplayName("Test deleteById method for submissionDAO")
+    public void deleteByIdSubmissionTest() {
+        Submission submission = PersistenciaInterface.createSubmission();
+        submission.setFilename("article.txt");
+        submission.setContent((new String("lots of data")).getBytes(StandardCharsets.UTF_8));
+        submission.setDate(LocalDate.now());
+
+        submissionDAO.save(submission);
+
+
+        Long id = submission.getId();
+        submissionDAO.deleteById(id);
+        Assertions.assertEquals(submissionDAO.findById(id), Optional.empty());
+    }
+
+    @Test
+    @DisplayName("Test findById method for submissionDAO")
+    public void findByIdSubmissionTest() {
+        Submission submission = PersistenciaInterface.createSubmission();
+        submission.setFilename("article.txt");
+        submission.setContent((new String("lots of data")).getBytes(StandardCharsets.UTF_8));
+        submission.setDate(LocalDate.now());
+
+        submissionDAO.save(submission);
+
+        Long id = submission.getId();
+        Assertions.assertEquals(submissionDAO.findById(id).get(), submission);
+    }
+
+    @Test
+    @DisplayName("Test findAll method for certificationDAO")
+    public void findAllSubmissionTest() {
+        Submission submission = PersistenciaInterface.createSubmission();
+        submission.setFilename("article.txt");
+        submission.setContent((new String("lots of data")).getBytes(StandardCharsets.UTF_8));
+        submission.setDate(LocalDate.now());
+
+        submissionDAO.save(submission);
+
+        List<Submission> submissions = submissionDAO.findAll();
+        Assertions.assertFalse(submissions.isEmpty());
     }
 }
