1625e6a9133433c3a209e2f799878d0d37415703
italan <italanleal@gmail.com>
2024-11-29 09:08:42 -0300

    Update EnvConfig.java

--- persistencia/src/main/java/br/upe/util/persistencia/EnvConfig.java
+++ persistencia/src/main/java/br/upe/util/persistencia/EnvConfig.java
@@ -3,14 +3,20 @@ package br.upe.util.persistencia;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvConfig {
-    private static final Dotenv env;
-    static {
+    private Dotenv env;
+    public static String get(String key) {
+        return env.get(key);
+    }
+    public EnvConfig(boolean develop){
         env = Dotenv.configure()
                 .directory("..")
                 .filename(".env")
                 .load();
     }
-    public static String get(String key) {
-        return env.get(key);
+    public EnvConfig(){
+        env = Dotenv.configure()
+                .directory(".")
+                .filename(".env")
+                .load();
     }
 }
