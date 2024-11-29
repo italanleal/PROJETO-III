4ae935ae123c66b497a010900de2d3289f62cbb2
italan <italanleal@gmail.com>
2024-11-29 09:18:21 -0300

    Update EnvConfigDiffblueTest.java

--- persistencia/src/test/java/br/upe/util/persistencia/EnvConfigDiffblueTest.java
+++ persistencia/src/test/java/br/upe/util/persistencia/EnvConfigDiffblueTest.java
@@ -8,23 +8,25 @@ public class EnvConfigDiffblueTest {
     /**
      * Test {@link EnvConfig#get(String)}.
      */
+    EnvConfig env = new EnvConfig(true);
+    
     @Test
     @DisplayName("Test get(DB_URL)")
     void testGetDB_URL() {
         // Arrange, Act and Assert
-        Assertions.assertNotNull(EnvConfig.get("DB_URL"));
+        Assertions.assertNotNull(envg.get("DB_URL"));
     }
     @Test
     @DisplayName("Test get(DB_USER)")
     void testGetDB_USER() {
         // Arrange, Act and Assert
-        Assertions.assertNotNull(EnvConfig.get("DB_USER"));
+        Assertions.assertNotNull(env.get("DB_USER"));
     }
     @Test
     @DisplayName("Test get(DB_PASSWORD)")
     void testGetDB_PASSWORD() {
         // Arrange, Act and Assert
-        Assertions.assertNotNull(EnvConfig.get("DB_PASSWORD"));
+        Assertions.assertNotNull(env.get("DB_PASSWORD"));
     }
 
 }
